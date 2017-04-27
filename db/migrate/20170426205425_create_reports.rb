60fc1960010d4dc4c8a1c77868563ab7a09ffe37
Jonathan Batista <jonathan.b.d.o.s@gmail.com>
2017-04-27 09:55:28 -0300

    Remove foreign_key from references on report

--- db/migrate/20170426205425_create_reports.rb
+++ db/migrate/20170426205425_create_reports.rb
@@ -1,8 +1,8 @@
 class CreateReports < ActiveRecord::Migration[5.0]
   def change
     create_table :reports do |t|
-      t.references :infected, foreign_key: true
-      t.references :by, foreign_key: true
+      t.references :infected
+      t.references :by
 
       t.timestamps
     end
